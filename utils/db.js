5365daf422dc273748a8f8b22e547b589aad500a
Tophan Ray <93485734+tophankumarray@users.noreply.github.com>
2025-05-14 22:12:52 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -9,10 +9,7 @@ console.log(process.env.MONGO_URI)
 
 
 const databaseConnection = () => {
-     mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-     }).then(()=>{
+     mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("mongodb connected successfully");
     }).catch((error)=>{
         console.log(error);
